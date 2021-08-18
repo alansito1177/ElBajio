879747c8ed67654ee05f557f906a6d5d65be04a0
Alan Avellaneda <alansito1177@gmail.com>
2021-08-18 10:54:40 -0400

    pushing images

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,20 @@
 import food from "../images/food.jpg";
 import logo from "../images/logo.png";
+import fish from "../images/fish.jpg";
 
 const Home = () => {
     return (
-    <div class="">
-        <div class="fluid-container">
+    <div>
+        <div class="container">
             <img src={logo} class="img-fluid p-3" />
-            
+            <div class="row">
+                <div class="card bg-dark">
+                    <img src={fish} class="card-img-top" alt="fish"/>
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>)
 }
