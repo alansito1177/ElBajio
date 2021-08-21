46e75239d5ee20933f32f2d57a31a2650274e424
Alan Avellaneda <alansito1177@gmail.com>
2021-08-21 04:18:34 -0400

    fixxed contact

--- src/components/contactUs.js
+++ src/components/contactUs.js
@@ -5,10 +5,10 @@ import bg from "../images/bg.png";
 const contact = () => {
     return (
         <div class="container">
-        <img src={logo} class="m-5 img-fluid" />
-        <h1 class="p-3"> Call us at <a href="tel:7068501191"> (706) 850-1191</a> </h1>
-        <h1 class="p-4">We Hope you<br/> enjoyed your meal</h1>
-        <img src={bg} class="m-5 img-fluid"/>
+        <img src={logo} class=" mt-3 mb-5 img-fluid" />
+        <h1 class="p-5"> Call us at <br/> <a href="tel:7068501191"> (706) 850-1191</a> </h1>
+        <h1 class="p-5">We Hope you<br/> enjoyed your meal</h1>
+        <img src={bg} class="img-fluid mt-5 mb-5"/>
         </div>
     )
 }
